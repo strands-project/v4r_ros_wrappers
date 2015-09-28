255fb2258ceb84231a1fe718e66b0e1407b22bda
Thomas Fäulhammer <faeulhammer@acin.tuwien.ac.at>
2015-09-28 10:45:43 +0200

    using deprecated pcl addcoordinatesystem function again as otherwise jenkins complains

--- object_classifier/src/test.cpp
+++ object_classifier/src/test.cpp
@@ -211,7 +211,7 @@ public:
         {
             vis_.reset(new pcl::visualization::PCLVisualizer("classifier visualization"));
         }
-        vis_->addCoordinateSystem(0.2f, "co", 0);
+        vis_->addCoordinateSystem(0.2f, 0);
         float text_scale = 0.010f;
         pcl::PointCloud<pcl::PointXYZRGB>::Ptr pClassifiedPCl (new pcl::PointCloud<pcl::PointXYZRGB>());
         pcl::copyPointCloud(*sceneXYZ_, *pClassifiedPCl);
