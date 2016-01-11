12deeb828c1b6091004f0f87db13afe0c5ffb1f0
Thomas Fäulhammer <faeulhammer@acin.tuwien.ac.at>
2016-01-11 11:35:29 +0100

    use old pcl grabber to be conform with PCL version 1.7

--- segment_and_classify/src/test.cpp
+++ segment_and_classify/src/test.cpp
@@ -78,8 +78,7 @@ public:
 
     bool callUsingFiles()
     {
-        std::vector<std::string> test_cloud;
-        v4r::io::getFilesInDirectory(directory_, test_cloud, "", ".*.pcd", false);
+        std::vector<std::string> test_cloud = v4r::io::getFilesInDirectory(directory_, ".*.pcd", false);
         for(size_t i=0; i < test_cloud.size(); i++)
         {
             pcl::PointCloud<PointT> cloud;
